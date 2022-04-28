a3d98665551051e376d7fe1d42cb114e8a64bda5
srinivasaraodannana <saikiran.dannana@gmail.com>
2022-04-28 18:33:41 +0530

    functional digital clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,88 @@
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState<Date>(new Date());
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(timeInterval);
+    };
+  }, []);
+
   return (
     <div className='container'>
-      <div>
-        <ul className='clock'>
-          <li className='numbers'>
-            <span>1</span>
-          </li>
-          <li className='numbers'>
-            <span>2</span>
-          </li>
-          <li className='numbers'>
-            <span>3</span>
-          </li>
-          <li className='numbers'>
-            <span>4</span>
-          </li>
-          <li className='numbers'>
-            <span>5</span>
-          </li>
-          <li className='numbers'>
-            <span>6</span>
-          </li>
-          <li className='numbers'>
-            <span>7</span>
-          </li>
-          <li className='numbers'>
-            <span>8</span>
-          </li>
-          <li className='numbers'>
-            <span>9</span>
-          </li>
-          <li className='numbers'>
-            <span>10</span>
-          </li>
-          <li className='numbers'>
-            <span>11</span>
-          </li>
-          <li className='numbers'>
-            <span>12</span>
-          </li>
-          <li className='hours'>
-            <span>hour</span>
-          </li>
-          <li className='minutes'>
-            <span>minute</span>
-          </li>
-          <li className='seconds'>
-            <span>second</span>
-          </li>
-        </ul>
+      <div className='clock-container'>
+        <div>
+          <ul className='clock'>
+            <li className='numbers'>
+              <span>1</span>
+            </li>
+            <li className='numbers'>
+              <span>2</span>
+            </li>
+            <li className='numbers'>
+              <span>3</span>
+            </li>
+            <li className='numbers'>
+              <span>4</span>
+            </li>
+            <li className='numbers'>
+              <span>5</span>
+            </li>
+            <li className='numbers'>
+              <span>6</span>
+            </li>
+            <li className='numbers'>
+              <span>7</span>
+            </li>
+            <li className='numbers'>
+              <span>8</span>
+            </li>
+            <li className='numbers'>
+              <span>9</span>
+            </li>
+            <li className='numbers'>
+              <span>10</span>
+            </li>
+            <li className='numbers'>
+              <span>11</span>
+            </li>
+            <li className='numbers'>
+              <span>12</span>
+            </li>
+            <li className='hours'>
+              <span>hour</span>
+            </li>
+            <li className='minutes'>
+              <span>minute</span>
+            </li>
+            <li className='seconds'>
+              <span>second</span>
+            </li>
+          </ul>
+        </div>
         <div>
           <ul className='digital-clock'>
-            <li className='digital-hours'>10</li>
-            <li className='digital-minutes'>12</li>
-            <li className='digital-seconds'>22</li>
+            <li className='digital-hours'>
+              <span>{format(date, 'hh')}</span>
+              <span className='unit'>H</span>
+            </li>
+            <li className='digital-minutes'>
+              <span>{format(date, 'mm')}</span>
+              <span className='unit'>M</span>
+            </li>
+            <li className='digital-seconds'>
+              <span>{format(date, 'ss')}</span>
+              <span className='unit'>S</span>
+            </li>
+            <li className='digital-seconds'>
+              <span>{format(date, 'a')}</span>
+            </li>
           </ul>
         </div>
       </div>
